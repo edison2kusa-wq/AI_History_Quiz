a641c69d0a6071798692d71a143c72c5f23969a1
Edison <edison2kusa@gmail.com>
2026-07-28 20:06:16 +0900

    관리자 문제 저장 오류 수정

--- admin/admin.js
+++ admin/admin.js
@@ -3,6 +3,13 @@
 // admin.js
 // ===============================
 
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+
+let currentPage = 1;
+
 // 전역 변수
 let currentPage = 1;
 const pageSize = 20;
